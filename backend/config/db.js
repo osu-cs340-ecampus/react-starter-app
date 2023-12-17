1b8d2c2125167e4675fbb6ea132c88a60e672951
Devin Daniels <devingdaniels@gmail.com>
2023-12-17 17:17:11 -0500

    updating readme

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,16 +15,4 @@ const pool = mysql
   })
   .promise();
 
-const testServerConnection = async () => {
-  const response = await pool.query("SELECT 1 + 1 AS solution");
-  if (response) {
-    console.log("Connected to the database!");
-    console.log("Anwser:", response[0][0].solution);
-  } else {
-    console.log("Error connecting to the database!");
-  }
-};
-
-testServerConnection();
-
 module.exports = pool;
